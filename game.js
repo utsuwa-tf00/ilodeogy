59eff4a11618b708391a5d35e8ad8ac5da52986e
utsuwa-tf00 <77259184+utsuwa-tf00@users.noreply.github.com>
2021-01-13 01:56:12 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,19 @@
 // JavaScript Document
 
+// スクロール関連メソッド
+var  scroll_control = function(event) {
+    event.preventDefault();
+}
+
+// スクロール禁止
+var  no_scroll = function() {
+    // PCでのスクロール禁止
+    document.addEventListener("mousewheel", scroll_control, { passive: false });
+    // スマホでのタッチ操作でのスクロール禁止
+    document.addEventListener("touchmove", scroll_control, { passive: false });
+}
+no_scroll();
+
 
 
 
